3d91c26113da78f14139ccbc415b295b027d6f18
ngthu1995 <ngthu1995@gmail.com>
2024-08-31 23:07:04 -0400

    add initial sign in, and home page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
-  ];
   return (
-    <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
+      </Route>
+    </Routes>
   );
 };
+
 export default App;
